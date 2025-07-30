99f498f548b6d2cebd8f0cc15ebb241e1ea6b872
ValerioC <valerio.colantonio@gmail.com>
2025-07-30 16:39:56 +0200

    webhookUrl test

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
     import('https://cdn.jsdelivr.net/npm/@n8n/chat/+esm').then(({ createChat }) => {
         createChat({
             element: '#n8n-chat',
-            backendUrl: 'https://n8n-work1.duckdns.org/webhook/620545ad-cc0e-4808-aa3e-4377e520ea45/chat'
+            webhookUrl: 'https://n8n-work1.duckdns.org/webhook/620545ad-cc0e-4808-aa3e-4377e520ea45/chat'
         });
     });
 });
